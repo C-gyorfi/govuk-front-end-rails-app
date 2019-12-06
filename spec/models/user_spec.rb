da86701eb359490ee7f7f0edb99750be8ff84797
csabagyorfi <csaba@madetech.com>
2019-12-06 15:29:59 +0000

    Add validations for user table

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(name: 'Bob', phone: '00000') }
+
+  describe 'a valid user' do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :phone }
+  end
 end
